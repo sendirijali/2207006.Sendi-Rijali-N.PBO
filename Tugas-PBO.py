b4c399443d5f083b60323466b4d6f4db09881260
sendirijali <nurohmansendi003@gmail.com>
2023-10-07 12:01:34 +0700

    Tugas PBO

---
+++ Tugas-PBO.py
@@ -0,0 +1,40 @@
+kalimat = "ini adalah kalimat"
+paragraf = """ini adalah paragraf.
+paragraf terdiri dari beberapa baris."""
+print(kalimat)
+print(paragraf)
+
+
+
+kalimat = "abfdkjasbfjdsbfjabfjfaj"
+print(kalimat[:5])
+print(kalimat[5:10])
+print(kalimat[21:])
+print(kalimat[-10:-1])
+print(kalimat[-10:])
+
+
+
+kalimat = "Halo, semua. Saya adalah programer Python."
+print(kalimat.upper())
+print(kalimat.lower())
+print(kalimat.capitalize())
+print(kalimat.title())
+print(kalimat.swapcase())
+print(kalimat.replace())
+print(kalimat.replace("Python", "Java"))
+print(kalimat.replace("a", "i"))
+print(kalimat.strip())
+print(kalimat.split('.'))
+
+
+
+kalimat ="Halo, semua. Saya adalah programer python."
+tambahan = "Saya sedang belajar python. Mohon bimbingannya."
+print(kalimat + tambahan)
+
+
+
+kalimat = "Halo, semua. Saya adalah programer \"python\"."
+tambahan = "Saya sedang belajar \"python\". Mohon bimbingannya."
+print(kalimat + tambahan)
