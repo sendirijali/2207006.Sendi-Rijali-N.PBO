c0351d974970dc9781eedccffedc12ed5789af63
sendirijali <147225943+sendirijali@users.noreply.github.com>
2023-12-18 09:12:42 +0700

    Add files via upload

---
+++ deletemhs.php
@@ -0,0 +1,10 @@
+<?php
+include 'latphp.php';
+$nim=$_GET['nim'];
+
+$simpandata=("delete from mahasiswa where nim='$nim'");
+mysqli_query($koneksi,$simpandata);
+
+if ($simpandata) {header('Location:reportmhs.php?Sukses');}
+else header('Location:reportmhs.php?Gagal');
+?>
